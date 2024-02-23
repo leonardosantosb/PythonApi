95ab1352bf66072986f0d58db5e7b15a2963117a
leonardosantosb <leonardosantosbasilio@gmail.com>
2024-02-23 16:13:13 -0300

    testes se erro

--- tests/test_app.py
+++ tests/test_app.py
@@ -67,3 +67,23 @@ def test_delete_user(client):
 
     assert response.status_code == 200
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_update_user_error(client):
+    response = client.put(
+        '/users/0',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_user_error(client):
+    response = client.delete('/users/0')
+
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
